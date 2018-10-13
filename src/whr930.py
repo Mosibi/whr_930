937df91fbf79626339d5991fc16b2834d85ee53e
Richard Arends <github@mosibi.nl>
2018-10-13 20:14:58 +0200

    Moved whr930 to src dir and did a rewrite from python2 to python3

--- src/whr930.py
+++ src/whr930.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -13,29 +13,29 @@ import time
 import serial
 
 def debug_msg(message):
-    if debug == True:
-        print '{0} DEBUG: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message)
+    if debug is True:
+        print('{0} DEBUG: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
 
 def warning_msg(message):
-    print '{0} WARNING: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message)
+    print('{0} WARNING: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
 
 def info_msg(message):
-    print '{0} INFO: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message)
+    print('{0} INFO: {1}'.format(time.strftime("%d-%m-%Y %H:%M:%S", time.gmtime()), message))
 
 def debug_data(serial_data):
-    print 'Ack           : {0} {1}'.format(serial_data[0], serial_data[1])
-    print 'Start         : {0} {1}'.format(serial_data[2], serial_data[3])
-    print 'Command       : {0} {1}'.format(serial_data[4], serial_data[5])
-    print 'Nr data bytes : {0} (integer {1})'.format(serial_data[6], int(serial_data[6], 16))
+    print('Ack           : {0} {1}'.format(serial_data[0], serial_data[1]))
+    print('Start         : {0} {1}'.format(serial_data[2], serial_data[3]))
+    print('Command       : {0} {1}'.format(serial_data[4], serial_data[5]))
+    print('Nr data bytes : {0} (integer {1})'.format(serial_data[6], int(serial_data[6], 16)))
 
     n = 1
     while n <= int(serial_data[6], 16):
-        print 'Data byte {0}   : Hex: {1}, Int: {2}, Array #: {3}'.format(n, serial_data[n+6], int(serial_data[n + 6], 16), n + 6)
+        print('Data byte {0}   : Hex: {1}, Int: {2}, Array #: {3}'.format(n, serial_data[n+6], int(serial_data[n + 6], 16), n + 6))
 
         n += 1
     
-    print 'Checksum      : {0}'.format(serial_data[-2])
-    print 'End           : {0} {1}'.format(serial_data[-1], serial_data[-0])        
+    print('Checksum      : {0}'.format(serial_data[-2]))
+    print('End           : {0} {1}'.format(serial_data[-1], serial_data[-0]))
 
 def on_message(client, userdata, message):
     if message.topic == 'house/2/attic/wtw/set_ventilation_level':
@@ -48,17 +48,17 @@ def on_message(client, userdata, message):
         debug_msg("message received: topic: {0}, payload: {1}, userdata: {2}".format(message.topic, message.payload, userdata))
 
 def publish_message(msg, mqtt_path):                                                                      
-    mqttc.publish(mqtt_path, payload=msg, qos=0, retain=False)
+    mqttc.publish(mqtt_path, payload=msg, qos=0, retain=True)
     time.sleep(0.1)
     debug_msg('published message {0} on topic {1} at {2}'.format(msg, mqtt_path, time.asctime(time.localtime(time.time()))))
 
 def serial_command(cmd):
     data = []
     ser.write(cmd)
-    time.sleep(1)
+    time.sleep(2)
 
     while ser.inWaiting() > 0:
-        data.append(ser.read(1).encode('hex'))
+        data.append(ser.read(1).hex())
 
     if len(data) > 0:
         return data
@@ -67,13 +67,13 @@ def serial_command(cmd):
 
 def set_ventilation_level(nr):
     if nr == 0:
-        data = serial_command("\x07\xF0\x00\x99\x01\x01\x48\x07\x0F")
+        data = serial_command(b'\x07\xF0\x00\x99\x01\x01\x48\x07\x0F')
     elif nr == 1:
-        data = serial_command("\x07\xF0\x00\x99\x01\x02\x49\x07\x0F")
+        data = serial_command(b'\x07\xF0\x00\x99\x01\x02\x49\x07\x0F')
     elif nr == 2:
-        data = serial_command("\x07\xF0\x00\x99\x01\x03\x4A\x07\x0F")
+        data = serial_command(b'\x07\xF0\x00\x99\x01\x03\x4A\x07\x0F')
     elif nr == 3:
-        data = serial_command("\x07\xF0\x00\x99\x01\x04\x4B\x07\x0F")
+        data = serial_command(b'\x07\xF0\x00\x99\x01\x04\x4B\x07\x0F')
 
     if data:
         if ( data[0] == '07' and data[1] == 'f3' ):
@@ -84,7 +84,7 @@ def set_ventilation_level(nr):
         warning_msg('Changing the ventilation to {0} went wrong, did not receive an ACK after the set command'.format(nr))
 
 def get_temp():
-    data = serial_command("\x07\xF0\x00\x0F\x00\xBC\x07\x0F")
+    data = serial_command(b'\x07\xF0\x00\x0F\x00\xBC\x07\x0F')
 
     if data == None:
         warning_msg('get_temp function could not get serial data')
@@ -102,7 +102,7 @@ def get_temp():
         debug_msg('OutsideAirTemp: {0}, SupplyAirTemp: {1}, ReturnAirTemp: {2}, ExhaustAirTemp: {3}'.format(OutsideAirTemp, SupplyAirTemp, ReturnAirTemp, ExhaustAirTemp))
 
 def get_ventilation_status():
-    data = serial_command("\x07\xF0\x00\xCD\x00\x7A\x07\x0F")
+    data = serial_command(b'\x07\xF0\x00\xCD\x00\x7A\x07\x0F')
 
     if data == None:
         warning_msg('get_ventilation_status function could not get serial data')
@@ -128,7 +128,7 @@ def get_fan_status():
     # Checksum: 0xB8 (0x00 0x0B) = 0 + 11 + 0 + 173 = 184
     # End: 0x07 0x0F
 
-    data = serial_command("\x07\xF0\x00\x0B\x00\xB8\x07\x0F")
+    data = serial_command(b'\x07\xF0\x00\x0B\x00\xB8\x07\x0F')
 
     if data == None:
         warning_msg('function get_fan_status could not get serial data')
@@ -153,7 +153,7 @@ def get_filter_status():
     # Checksum: 0x86 (0x00 0xD9) = 0 + 217 + 0 + 173 = 390
     # End: 0x07 0x0F
 
-    data = serial_command("\x07\xF0\x00\xD9\x00\x86\x07\x0F")
+    data = serial_command(b'\x07\xF0\x00\xD9\x00\x86\x07\x0F')
 
     if data == None:
         warning_msg('get_filter_status function could not get serial data')
